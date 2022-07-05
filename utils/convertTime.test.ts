f19ce80ff5ccdb48870ce51bf378b456b72e63cb
Tushar Indurjeeth <tusharindurjeeth@protonmail.com>
2022-07-06 01:26:29 +0200

    Updated tests

--- utils/convertTime.test.ts
+++ utils/convertTime.test.ts
@@ -1,11 +1,15 @@
-import { convertTime } from './convertTime';
+import { convertTime, convertTimeW } from './convertTime';
 
 describe('convertTime()', () => {
   it('should return a string', () => {
-    const { centiSeconds, seconds, minutes } = convertTime(12332);
+    const { centiSeconds, seconds, minutes } = convertTime(8480);
 
     expect(typeof centiSeconds).toEqual('string');
     expect(typeof seconds).toEqual('string');
     expect(typeof minutes).toEqual('string');
+
+    expect(centiSeconds).toEqual('48');
+    expect(seconds).toEqual('08');
+    expect(minutes).toEqual('00');
   });
 });
